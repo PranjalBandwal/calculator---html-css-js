2c6e7d880c7f6e1c854b530806fd6b49663ae239
Pranjal Bandwal <bandwal9@gmail.com>
2024-02-07 21:24:00 +0530

    project-1
    bg-color picker using switch case

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-const button = document.querySelectorAll('.button')
-console.log(button)
+const buttons = document.querySelectorAll('.button')
+const string = ""
+string = document.querySelector('#string')
+buttons.forEach(function(btn){
+    btn.addEventListener('click', function(obj){
+        string=obj.target.value
+        
+    })
+})
